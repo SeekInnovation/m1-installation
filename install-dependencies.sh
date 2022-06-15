40b2b44559c998ca3b6f9a9d501bcf7e28ec27f4
Fabio Moretti <fmoretti981@gmail.com>
2022-06-15 16:13:37 +0200

    fix

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -18,7 +18,7 @@
 # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> ~/.zprofile
-# eval $(/opt/homebrew/bin/brew shellenv)
+eval $(/opt/homebrew/bin/brew shellenv)
 
 #Upgrade installed formulae
 brew upgrade
