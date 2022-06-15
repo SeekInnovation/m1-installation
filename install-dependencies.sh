9408ba759d6c3d7e55b830a7f5b488bc0a0d26e4
Fabio Moretti <fmoretti981@gmail.com>
2022-06-15 15:52:10 +0200

    tmp

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-softwareupdate --install-rosetta
+#install rosetta
+# printf "A\n\n" | softwareupdate --install-rosetta
 
 # download and install docker 
-curl 'https://desktop.docker.com/mac/main/arm64/Docker.dmg' --ouput ~/Downloads/docker.dmg
-sudo hdiutil attach ~/Downloads/Docker.dmg
-cp -r /Volumes/Docker/Docker.app /Applications/
-sudo hdiutil unmount /Volumes/Docker
-rm ~/Downloads/Docker.dmg
+# curl --output ~/Downloads/docker.dmg 'https://desktop.docker.com/mac/main/arm64/Docker.dmg'
+# sudo hdiutil attach ~/Downloads/Docker.dmg
+# cp -r /Volumes/Docker/Docker.app /Applications/
+# sudo hdiutil unmount /Volumes/Docker
+# rm ~/Downloads/Docker.dmg
+
+# install Github, VSCODE, postman, insomnia
+curl -s 'https://api.macapps.link/de/github-vscode-postman-insomnia' | sh
