65f6cf244d3c0cb296190883b29a667d543f0d92
Fabio Moretti <fmoretti981@gmail.com>
2022-06-15 15:35:38 +0200

    fix

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yes | softwareupdate --install-rosetta
+printf "A\n\n" | softwareupdate --install-rosetta
 
 # download and install docker 
 curl -o ~/Downloads/docker.dmg https://desktop.docker.com/mac/main/arm64/Docker.dmg --ouput ~/Downloads/Docker.dmg
