3f8ecf2066c11b4decb4f68d8e95d57157a6e09e
Fabio Moretti <fmoretti981@gmail.com>
2022-06-15 15:44:16 +0200

    fix

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 softwareupdate --install-rosetta
 
 # download and install docker 
-curl --ouput ~/Downloads/docker.dmg https://desktop.docker.com/mac/main/arm64/Docker.dmg
+curl 'https://desktop.docker.com/mac/main/arm64/Docker.dmg' --ouput ~/Downloads/docker.dmg
 sudo hdiutil attach ~/Downloads/Docker.dmg
 cp -r /Volumes/Docker/Docker.app /Applications/
 sudo hdiutil unmount /Volumes/Docker
