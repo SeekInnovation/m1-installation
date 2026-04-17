4c724f8fad3b103ec92c60b2dd5eb8dac0a9bd7d
Dominik Simon <dominik@dsimon.dev>
2026-04-17 12:33:02 +0200

    update install script
    - remove chrome, installed by jamf
    - remove docker, causes issues and should be installed manually
    - fix homebrew completions

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,38 +1,21 @@
-#!/bin/sh
+#!/bin/bash
+set -eu
 
-touch ~/.zshrc
-
-# usage: installdmg https://example.com/path/to/pkg.dmg
-function installdmg {
-    set -x
-    tempd=$(mktemp -d)
-    curl $1 > $tempd/pkg.dmg
-    listing=$(sudo hdiutil attach $tempd/pkg.dmg | grep Volumes)
-    volume=$(echo "$listing" | cut -f 3)
-    if [ -e "$volume"/*.app ]; then
-      sudo cp -rf "$volume"/*.app /Applications
-    elif [ -e "$volume"/*.pkg ]; then
-      package=$(ls -1 "$volume" | grep .pkg | head -1)
-      sudo installer -pkg "$volume"/"$package" -target /
-    fi
-    sudo hdiutil detach "$(echo "$listing" | cut -f 1)"
-    rm -rf $tempd
-    set +x
-}
+# ensure .zshrc exists
+if [ ! -f "~/.zshrc" ]; then
+    echo "Creating ~/.zshrc..."
+    touch "~/.zshrc"
+fi
 
 # install rosetta
 softwareupdate --install-rosetta --agree-to-license
 
-# download and install docker 
-installdmg https://desktop.docker.com/mac/main/arm64/Docker.dmg
-
-# download and install chrome m1
-installdmg https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg
-
 # install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> ~/.zshrc
-eval $(/opt/homebrew/bin/brew shellenv)
+echo "
+# Enable Homebrew and custom shell completions
+autoload -Uz compinit
+compinit" >> ~/.zshrc
 
 # Upgrade installed formulae
 brew upgrade
