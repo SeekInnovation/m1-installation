30ca5560efb8bfb0a63fe27c952c90b18f5e9ddc
Dominik Simon <dominik.simon@learningsuite.io>
2026-04-17 12:38:29 +0200

    fix zshrc, add logs

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # m1-installation
 
 To run paste the following into the terminal:
+
 ```bash
-sh -c "$(curl -fsSL 'https://raw.githubusercontent.com/SeekInnovation/m1-installation/main/install-dependencies.sh')"
+bash -c "$(curl -fsSL 'https://raw.githubusercontent.com/SeekInnovation/m1-installation/main/install-dependencies.sh')"
 ```
 
 For tips and tricks and an overview on useful shortcuts, open the `docs` directory and read the relevant files.
--- install-dependencies.sh
+++ install-dependencies.sh
@@ -2,26 +2,30 @@
 set -eu
 
 # ensure .zshrc exists
-if [ ! -f "~/.zshrc" ]; then
-    echo "Creating ~/.zshrc..."
-    touch "~/.zshrc"
+if [ ! -f ~/.zshrc ]; then
+  echo "Creating ~/.zshrc..."
+  touch ~/.zshrc
 fi
 
 # install rosetta
+echo "Installing Rosetta 2..."
 softwareupdate --install-rosetta --agree-to-license
 
 # install homebrew
+echo "Installing Homebrew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo "
 # Enable Homebrew and custom shell completions
 autoload -Uz compinit
 compinit" >> ~/.zshrc
 
-# Upgrade installed formulae
+# upgrade installed formulae
 brew upgrade
 
-# Install dev tools
+# install dev tools
+echo "Installing development tools..."
 brew install git wget httpie jq ffmpeg yt-dlp
 
 # install nvm
+echo "Installing nvm..."
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
